822f07bbe4c7ad8bb044a3722711a0a9b3c5afce
gustopster <gustavogomes763@gmail.com>
2023-03-30 23:02:02 -0300

    Correção dos index e da ordem aleatoria da biblia.

--- src/Components/Versiculo/Versiculo.tsx
+++ src/Components/Versiculo/Versiculo.tsx
@@ -1,39 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import bibliaLivreData from "./biblialivre.json";
 import "./Versiculo.css";
+
 interface BibliaLivre {
-  id: string;
-  periodo: string;
-  nome: string;
-  abrev: string;
-  capitulos: {
+  id?: string;
+  periodo?: string;
+  nome?: string;
+  abrev?: string;
+  capitulos?: {
     [key: string]: string[];
   };
 }
+
 function Versiculo() {
-  const [verse, setVerse] = useState<BibliaLivre>({ id: '', periodo: '', nome: '', abrev: '', capitulos: {} });
-  const [bibliaLivre, setBibliaLivre] = useState<BibliaLivre[]>([]);
-  const [capitulosAleatorios, setCapitulosAleatorios] = useState<string[]>([]);
+  const bibliaLivreDataTyped: BibliaLivre[] = (bibliaLivreData as BibliaLivre[]);
+  const [versiculo, setVersiculo] = useState<string>("");
+  const [referencia, setReferencia] = useState<string>("");
+
   useEffect(() => {
-    const bibliaLivreDataTyped = bibliaLivreData as BibliaLivre[];
-    setBibliaLivre(bibliaLivreDataTyped);
+    const randomBookIndex = Math.floor(Math.random() * bibliaLivreDataTyped.length);
+    const randomBook = bibliaLivreDataTyped[randomBookIndex];
+    const randomChapterIndex = Math.floor(Math.random() * Object.keys(randomBook.capitulos || {}).length);
+    const randomChapterKey = Object.keys(randomBook.capitulos || {})[randomChapterIndex];
+    const randomChapter = randomBook.capitulos ? randomBook.capitulos[randomChapterKey] : [];
+    const randomVerseIndex = Math.floor(Math.random() * randomChapter.length);
+    const randomVerse = randomChapter[randomVerseIndex];
+    setVersiculo(randomVerse);
+    setReferencia(`${randomBook.nome} ${parseInt(randomChapterKey) + 1}:${randomVerseIndex + 1}`);
   }, []);
-  useEffect(() => {
-    const randomNumber = Math.floor(Math.random() * bibliaLivre.length);
-    const verseData = bibliaLivre[randomNumber];
-    if (verseData) {
-      setVerse(verseData);
-      const capitulos = Object.values(verseData.capitulos);
-      const capitulosAleatorios = capitulos.map(cap => cap[Math.floor(Math.random() * cap.length)]);
-      setCapitulosAleatorios(capitulosAleatorios);
-    }
-  }, [bibliaLivre]);
+
   return (
     <div className='versiculoDiv'>
-      <h3>Versículo do Dia</h3>
-      <h5>{`Versiculo retirado do livro de: "${verse.nome}"`}</h5>
-      <p className='versiculoP'>{`${capitulosAleatorios.join(', ')}`}</p>
+      <h1>Seu Versículo</h1>
+      <h2> Livro de: {referencia}</h2>
+      <h3>{versiculo}</h3>
     </div>
   );
 }
+
 export default Versiculo;
