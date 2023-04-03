e0fcd9f2791d54a05ad62f139f650680a0883ac2
gustavogomes <gustavo.g.souza.prestserv@petrobras.com.br>
2023-04-03 09:00:16 -0300

    Feito o botao resetar do contador

--- src/Pages/Contador/Contador.tsx
+++ src/Pages/Contador/Contador.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, addDoc, collection } from 'firebase/firestore';
 import { dadosFirebase } from '../../services/firebaseServer';
 import { ContagemPessoas } from '../../types/ContagemPessoas';
 import { contagemObreiros } from '../../types/ContagemObreiros';
+
 const Contador = () => {
   const [obreiros, setObreiros] = useState<number>(0);
   const [pessoas, setPessoas] = useState<number>(0);
+
   useEffect(() => {
     async function getConfig() {
       const snapshotObreiros = await getDoc(doc(dadosFirebase, "contagemObreiros", "contador"));
@@ -19,6 +21,7 @@ const Contador = () => {
     }
     getConfig()
   }, [obreiros, pessoas]);
+
   const enviarContagem = () => {
     const novosDados = { resultado: pessoas + 1 };
     updateDoc(doc(dadosFirebase, "contagemPessoas", "contador"), novosDados)
@@ -27,20 +30,44 @@ const Contador = () => {
         console.error('Erro ao atualizar o documento:', error);
       });
   }
+
+  const resetarPessoas = async () => {
+    const confirmacao = window.confirm('Tem certeza que deseja resetar o número de pessoas?');
+    if (confirmacao) {
+      // salvar os dados antes de resetar
+      const dadosAntesDoReset = { resultado: pessoas + obreiros};
+      await addDoc(collection(dadosFirebase, "dadosAntesDoReset"), dadosAntesDoReset);
+
+      // resetar o número de pessoas
+      const novosDados = { resultado: 0 };
+      updateDoc(doc(dadosFirebase, "contagemPessoas", "contador"), novosDados)
+        .then(() => {
+          setPessoas(0);
+        })
+        .catch((error) => {
+          console.error('Erro ao atualizar o documento:', error);
+        });
+    }
+  }
   return (
     <>
       <div className='contadorDiv'>
-        <h1 className='totalPessoas'>Total: {obreiros + pessoas === NaN || null || undefined || "" ? 0 : obreiros + pessoas}</h1>
+        <h1 className='totalPessoas'>Total: {obreiros + pessoas}</h1>
         <h3> {obreiros} Obreiros na Igreja</h3>
         <h2> {pessoas} Pessoas na Igreja</h2>
         <div>
+          <button className='botaoContar' onClick={() => {
+            setPessoas(pessoas + 1);
+            enviarContagem();
+          }}>
+            Click para contar
+          </button>
         </div>
-        <button className='botaoContar' onClick={() => {
-          setPessoas(pessoas + 1);
-          enviarContagem();
-        }}>
-          Click para contar
-        </button>
+        <div>
+          <button className='botaoResetar' onClick={resetarPessoas}>
+            ResetarEZerar
+          </button>
+        </div>
       </div>
     </>
   );
